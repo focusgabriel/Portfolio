feea580a9f7ecf26921d1be9e8752f9e18a0ab06
focusgabriel <focusgabriel@users.noreply.github.com>
2026-09-09 23:17:35 +0100

    refactor: improve layout and styling of LabelCards component

--- src/components/LabelCards.tsx
+++ src/components/LabelCards.tsx
@@ -1,13 +1,15 @@
+/** @format */
+
 interface labelProps {
   content: string | string[];
 }
 
 const LabelCards = ({ content }: labelProps) => {
   return (
-    <div className="flex justify-between border border-[#c5d3e7] my-[4px] text-slate-400 rounded-[5px] px-[6px] pb-[2px] text-center text-[14px]">
-      <p>{content}</p>
+    <div className="flex items-center justify-center border border-[#c5d3e7] my-1 text-slate-400 rounded-md px-3 py-1 text-center text-[13px]">
+      <p className="truncate">{content}</p>
     </div>
-  )
-}
+  );
+};
 
-export default LabelCards
+export default LabelCards;
